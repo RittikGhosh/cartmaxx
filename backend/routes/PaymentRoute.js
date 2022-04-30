d15166b5a3e3ec5058c59d336f7be44a53eb39f1
Rittick Ghosh <rttckghsh89@gmail.com>
2022-04-30 21:26:23 +0530

    ff

--- backend/routes/PaymentRoute.js
+++ backend/routes/PaymentRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const {
     processPayment,
     sendStripeApiKey,
-} = require("../controllers/PaymentController");
+} = require("../controllers/paymentController");
 const router = express.Router();
 const { isAuthenticatedUser } = require("../middleware/auth");
 
